0817e74f4214ecc22b81eb503cf29fa8bc7a5fb5
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-15 16:10:19 +0000

    Test: raise an error when the maximum balance is exceeded

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Oystercard do
     it 'should return a new balance when monies are added' do
       expect{subject.top_up 1 }.to change { subject.balance }.by 1
     end
+
+    it 'should raise an error when the maximum balance is exceeded' do
+      expect { subject.top_up 91 }.to raise_error('Maximum balance is exceeded')
+    end
     
+
   end
 end
