15b9eb5064490a551f3a3b00c5937809a50e3c85
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-15 14:54:56 +0000

    Test: should respond to top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,21 @@
 require 'oystercard'
 
 RSpec.describe Oystercard do
-  it 'should have a balance of 0 by default' do
-  expect(subject.balance).to eq(0)
+
+
+    it 'should have a balance of 0 by default' do
+      expect(subject.balance).to eq(0)
+    end
+
+  describe '#top_up' do
+
+    it 'should respond to top_up method' do
+      expect(subject).to respond_to(:top_up)
+    end
+
+
+
+
+
   end
 end
