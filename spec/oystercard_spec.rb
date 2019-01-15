da31a3cc73272ea0e3e458c9b51c49b50d397d3d
ellie turnock <ebturnock@gmail.com>
2019-01-15 16:24:45 +0000

    Test: refactored test to include constant for max balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,23 +2,20 @@ require 'oystercard'
 
 RSpec.describe Oystercard do
 
-
     it 'should have a balance of 0 by default' do
       expect(subject.balance).to eq(0)
     end
 
   describe '#top_up' do
-
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'should return a new balance when monies are added' do
-      expect{subject.top_up 1 }.to change { subject.balance }.by 1
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
     end
 
     it 'should raise an error when the maximum balance is exceeded' do
-      expect { subject.top_up 91 }.to raise_error('Maximum balance is exceeded')
+      max = Oystercard::MAX_BALANCE
+      expect { subject.top_up max + 1 }.to raise_error("Maximum balance of £#{max}.00 is exceeded")
     end
-    
+
 
   end
 end
