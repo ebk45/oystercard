99f62d272a352cec7cb2849e4e32a085951a6a19
ellie turnock <ebturnock@gmail.com>
2019-01-16 14:24:53 +0000

    Test: should allow user to touch in/out and check if in journey

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Oystercard do
       max = Oystercard::MAX_BALANCE
       expect { subject.top_up max + 1 }.to raise_error("Maximum balance of £#{max}.00 is exceeded")
     end
+  end
 
   describe '#deduct' do
     it 'should deduct monies from the oystercard' do
@@ -22,6 +23,36 @@ RSpec.describe Oystercard do
     end
   end
 
+  describe '#in_journey' do
+    it 'should be false when a new card is created' do
+      expect(subject.in_journey?).to eq(false)
+    end
+  end
+
+  describe '#touch_in' do
+    it 'should touch in the card' do
+      expect(subject).to respond_to(:touch_in)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'should touch out the card' do
+      expect(subject).to respond_to(:touch_out)
+    end
+  end
+
+
+  context '1. Card state should change during journey' do
+    it 'should start journey when touched in' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+
+    it 'should end journey when touched out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq(false)
+    end
 
   end
 end
