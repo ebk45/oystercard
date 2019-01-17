d6df8f159e2d93ad450068923a95b97d21c799a8
ellie turnock <ebturnock@gmail.com>
2019-01-17 15:43:55 +0000

    added journey class and log functionality

---
+++ lib/journey.rb
@@ -0,0 +1,11 @@
+class Journey
+
+  def initialize
+    @entry_station = nil
+  end
+
+  def start(entry_station)
+    @entry_station = entry_station
+  end
+  
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,12 @@
+require 'journey'
+
+describe Journey do
+  let(:entry_station) { double("station", name: "aldgate", zone: 1)}
+
+  it '1. should log an entry station' do
+    expect(subject.start(entry_station)).to eq(entry_station)
+  end
+
+
+
+end
