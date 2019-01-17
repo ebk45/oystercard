5cd76d43a47499f4e45617229cb0166c258f474b
ellie turnock <ebturnock@gmail.com>
2019-01-17 15:04:53 +0000

    added station class and refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,31 +3,30 @@ class Oystercard
   MAX_BALANCE = 90
   MIN_BALANCE = 1
 
-  attr_reader :balance, :monies, :entry_station, :journeys
+  attr_reader :balance, :monies, :journeys
 
   def initialize(balance = 0)
     @journeys = []
     @balance = balance
-    @entry_station = []
   end
 
   def top_up(monies)
-    fail "Maximum balance of £#{MAX_BALANCE}.00 is exceeded" if (@balance + monies) > MAX_BALANCE
+    error_message = "Maximum balance of £#{MAX_BALANCE}.00 is exceeded"
+    fail error_message if (@balance + monies) > MAX_BALANCE
+
     @balance += monies
   end
 
-  def in_journey?
-    @entry_station != []
-  end
-
   def touch_in(entry_station)
-    fail "You need to have a minimum of £#{MIN_BALANCE}.00 to travel" unless @balance >= MIN_BALANCE
-    @entry_station << entry_station
+    error_message = "You need to have a minimum of £#{MIN_BALANCE}.00 to travel"
+    fail error_message unless @balance >= MIN_BALANCE
+
+    @journeys << ({ entry: entry_station, exit: nil })
   end
 
   def touch_out(exit_station)
     deduct(MIN_BALANCE)
-    @journeys << { @entry_station.pop => exit_station }
+    @journeys.last[:exit] = exit_station
   end
   private
 
